8c42f0c5be0ed89f3036801b8dafd18a8531a55b
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-04-17 06:27:24 +0530

    Added Profile Info
    Added html,css and toggle js for profile information

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 var searchIconWhite = document.getElementById('search-icon-white');
 var searchIconClose = document.getElementById('search-icon-close');
 var searchInput = document.getElementById('search-input');
+var profileIcon = document.getElementById('profile-icon');
+var profileExpandOverlay = document.getElementById('profile-overlay');
 var header = document.getElementById('header');
 const SEARCH_CLASS = 'display-search';
-
+const PROFILE_CLASS = 'display-profile';
 
 if(searchInput.value) {
     openSearchBox();
 }
 
 searchIconWhite.addEventListener('click', openSearchBox);
+profileIcon.addEventListener('click', openProfileExpand);
 
 function openSearchBox() {
     header.classList.add(SEARCH_CLASS)
@@ -22,4 +25,16 @@ function closeSearchBox() {
     header.classList.remove(SEARCH_CLASS)
     searchIconWhite.addEventListener('click', openSearchBox);
     searchIconClose.removeEventListener('click', closeSearchBox);
+}
+
+function openProfileExpand() {
+    header.classList.add(PROFILE_CLASS);
+    profileExpandOverlay.addEventListener('click', closeProfileExpand);
+    profileIcon.removeEventListener('click', openProfileExpand);
+}
+
+function closeProfileExpand() {
+    header.classList.remove(PROFILE_CLASS);    
+    profileIcon.addEventListener('click', openProfileExpand);
+    profileExpandOverlay.removeEventListener('click', closeProfileExpand);    
 }
