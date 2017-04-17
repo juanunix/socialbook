fb351961587f8c86ab329ff965d3c460671a169e
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-04-17 06:40:34 +0530

    Added Home Scroll
    Added Js for scrolling to top on home click
    Small styling changes on profile expand to make it more elevated

--- js/app.js
+++ js/app.js
@@ -38,3 +38,19 @@ function closeProfileExpand() {
     profileIcon.addEventListener('click', openProfileExpand);
     profileExpandOverlay.removeEventListener('click', closeProfileExpand);    
 }
+
+
+/* Home */
+const HOME_LINK_CLASS = 'home-link';
+
+var homeLinks = document.getElementsByClassName(HOME_LINK_CLASS);
+
+for (var index = 0; index < homeLinks.length; index++) {
+    var element = homeLinks.item(index);
+    element.addEventListener('click', function(event) {
+        if(window.scrollY > 0) {
+            event.preventDefault();
+            window.scrollTo(0, 0);
+        }
+    })
+}
